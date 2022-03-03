d4bcfe5101c942dfb31308905de5da023122bae6
thewarrior1220 <hatvanitamas09@gmail.com>
2022-03-03 16:00:02 +0100

    update

--- 3D/Assets/Scripts/TestMovement.cs
+++ 3D/Assets/Scripts/TestMovement.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class TestMovement : MonoBehaviour
 {
     public CharacterController controller;
+    //public Transform gameobject;
 
     public float speed = 15f;
     public float turnSmoothness = 0.1f;
+    public float targetAngle, angle;
     float turnSmoothVelocity;
     float x, y;
-    Vector3 direction;
+    public Vector3 direction;
     
     void Update()
     {
@@ -19,10 +21,11 @@ public class TestMovement : MonoBehaviour
         direction = new Vector3(x, 0f, y).normalized;
 
         if (direction.magnitude >= 0.1f) {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.z, targetAngle, ref turnSmoothVelocity, turnSmoothness);
+            targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg; //+ gameobject.eulerAngles.y
+            angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothness);
             transform.rotation = Quaternion.Euler(-90f, angle, 0f);
 
+            //Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(direction * speed * Time.deltaTime);
         }
     }
